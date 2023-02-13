a5fc90d813c5403e1f9e154b66b0a9945a43bdc3
PavelNosov1986 <99183133+PavelNosov1986@users.noreply.github.com>
2023-02-13 15:52:28 +0300

    in progress

--- backend/app.js
+++ backend/app.js
@@ -29,13 +29,15 @@ if (!process.env.JWTKEY) {
 
 app.use((req, res, next) => {
   const { method } = req;
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Credentials', true);
+  // }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
