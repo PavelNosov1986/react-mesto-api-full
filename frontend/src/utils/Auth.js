b6fb219fa144c41f6805f8570def7e1e06e1174a
PavelNosov1986 <99183133+PavelNosov1986@users.noreply.github.com>
2023-02-13 14:03:16 +0300

    in progress

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://auth.nomoreparties.co";
+const BASE_URL = "https://api.reactmestofull.nosovp.nomoredomainsclub.ru";
 
 function checkRes(res) {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
