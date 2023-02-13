0f61d6f1faeb79ca890a3605230421fd9994723e
PavelNosov1986 <99183133+PavelNosov1986@users.noreply.github.com>
2023-02-13 15:50:29 +0300

    in progress

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
